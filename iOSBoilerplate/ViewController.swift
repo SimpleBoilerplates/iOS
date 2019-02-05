3ec693aa110f5b41aebff1bd81930291e120b04b
sadman samee <sadman.tonmoy@gmail.com>
2019-02-05 09:08:29 +0600

    Initial Commit

---
+++ iOSBoilerplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSBoilerplate
+//
+//  Created by sadman samee on 5/2/19.
+//  Copyright © 2019 sadman samee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
